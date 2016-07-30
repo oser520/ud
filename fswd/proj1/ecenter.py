9cf9de76a8509fcb2be380b81e4fa90551f4f62e
Omar Serrano <oser520@gmail.com>
2016-07-29 21:38:28 -0700

    Add logic to process argument options

--- fswd/proj1/ecenter.py
+++ fswd/proj1/ecenter.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 
+import argparse
 import shelve
+import sys
+import time
+import fresh_tomatoes
 from media import Movie
 from omdb import get_movie_info
 from youtube_trailer import get_trailer_url
-import fresh_tomatoes
-import time
-import sys
 
 # default movies to search for and add to the movie trailer page
 movie_titles = [
@@ -16,13 +17,39 @@ movie_titles = [
     'her', 'adaptation'
 ]
 
-# list of movies that don't contain any errors
+# parse the arguments
+parser = argparse.ArgumentParser(description='Create Fresh Tomatoes Web Page')
+parser.add_argument('--dbfile', default='movie_trailer_db',
+    help='The database for the movie trailers')
+parser.add_argument('-f', '--file',
+    help='Input file to get movie titles from, with one title per line')
+parser.add_argument('-l', '--list',
+    help='List of movies separated by separator')
+parser.add_argument('-s', '--separator', default=',',
+    help='The separator used if a list of movies is provided')
+args = parser.parse_args()
+
+# open the database
+db = shelve.open(args.dbfile, writeback=True)
+
+# read titles from a file
+if args.file:
+    for title in open(args.file).readlines():
+        title = title.strip().lower()
+        movie_titles.append(title) if title
+
+# process titles in list
+if args.list:
+    titles = args.list.split(args.separator)
+    for title in titles:
+        title = title.strip().lower()
+        movie_titles.append(title) if title
+
+# list of movies to include in trailer web page
 movies = []
 
-db = shelve.open('movie_trailer_db', writeback=True)
-
+# process each movie title
 for title in movie_titles:
-    title = title.lower()
     print 'Searching for movie %s' % title
     if db.has_key(title):
        print 'Movie %s found in database via search title' % title
@@ -51,5 +78,4 @@ for title in movie_titles:
 
 db.close()
 fresh_tomatoes.open_movies_page(movies)
-
 sys.exit(0)
