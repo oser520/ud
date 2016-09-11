91671fb5ac96df0f2a3078ba77a849621d510d75
Omar Serrano <oser520@gmail.com>
2016-09-10 17:56:13 -0700

    Fix formatting and add default grunt task

--- fswd/proj2/Gruntfile.js
+++ fswd/proj2/Gruntfile.js
@@ -1,53 +1,42 @@
 module.exports = function(grunt) {
-	grunt.initConfig({
-	  imagemin: {
-	    png: {
-	      files: [
-	        {
-	          expand: true,
-	          cwd: 'img/',
-	          src: ['**/*.png'],
-	          dest: './img/compressed/',
-	          ext: '.png'
-	        }
-	      ]
-	    },
-	    jpg: {
-	      options: { progressive: true },
-	      files: [
-	        {
-	          expand: true,
-	          cwd: 'img/',
-	          src: ['**/*.jpg'],
-	          dest: './img/compressed',
-	          ext: '.jpg'
-	        }
-	      ]
-	    }
-	  },
+  grunt.initConfig({
+    imagemin: {
+      jpg: {
+        options: { progressive: true },
+        files: {
+          'production/img/chess.jgp': 'production/img/chess-small.jpg',
+          'production/img/hf-trading.jgp': 'production/img/hf-trading-small.jpg',
+          'production/img/movie-studio.jgp': 'production/img/movie-studio-small.jpg'
+        }
+      }
+    },
     responsive_images: {
       ostask: {
-	      options: {
-	        engine: 'im',
-	        concurrency: 3,
-	        newFilesOnly: true
-	      },
-	      files: [{
-	        expand: true,
-	        src: ['img/**.{jpg,png}'],
-	        dest: 'img/responsive'
-	      }]
+        options: {
+          engine: 'im',
+          concurrency: 3,
+          newFilesOnly: true,
+          sizes: [
+            {name: 'small', width: 320}
+          ]
+        },
+        files: [{
+          expand: true,
+          cwd: '.',
+          src: ['img/**.{jpg,png}'],
+          dest: 'production/'
+        }]
       }
     },
     cssmin: {
       target: {
-	      files: { 'css/style.min.css': 'css/style.css' }
+        files: { 'production/css/style.min.css': 'css/style.css' }
       }
     },
     htmlmin: {
       options: { collapseWhitespace: true },
       target: {
-	      files: { 'production/index.html': 'index.html' }
+        files: { 'production/index.html': 'index.html' }
       }
     },
     validation: {
@@ -57,7 +46,7 @@ module.exports = function(grunt) {
         relaxerror: ['Empty heading.']
       },
       files: {
-	       src: ['index.html']
+        src: ['index.html']
       }
     },
     csslint: {
@@ -65,19 +54,29 @@ module.exports = function(grunt) {
         src: ['css/style.css']
       }
     }
-	});
+  });
 	
-	grunt.loadNpmTasks('grunt-contrib-imagemin');
-	grunt.loadNpmTasks('grunt-responsive-images');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.loadNpmTasks('grunt-contrib-htmlmin');
-	grunt.loadNpmTasks('grunt-w3c-html-validation');
-	grunt.loadNpmTasks('grunt-contrib-csslint');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-w3c-html-validation');
+  grunt.loadNpmTasks('grunt-contrib-csslint');
 
-	grunt.registerTask('imgopt', ['imagemin']);
-	grunt.registerTask('sizeopt', ['responsive_images']);
-	grunt.registerTask('cssopt', ['cssmin']);
-	grunt.registerTask('htmlopt', ['htmlmin']);
-	grunt.registerTask('valid', ['validation']);
-	grunt.registerTask('css', ['csslint']);
+  grunt.registerTask('imgopt', ['imagemin']);
+  grunt.registerTask('sizeopt', ['responsive_images']);
+  grunt.registerTask('cssopt', ['cssmin']);
+  grunt.registerTask('htmlopt', ['htmlmin']);
+  grunt.registerTask('valid', ['validation']);
+  grunt.registerTask('css', ['csslint']);
+  grunt.registerTask('default',
+    [
+      'validation',
+      'csslint',
+      'htmlmin',
+      'cssmin',
+      'responsive_images',
+      'imagemin'
+    ]
+  );
 };
