fdbf5c6ccada9fb6c4d014ae1e1e0cdbf96d3f43
Omar Serrano <oser520@gmail.com>
2016-09-11 11:03:13 -0700

    Change order of tasks to be the same as for default

--- fswd/proj2/Gruntfile.js
+++ fswd/proj2/Gruntfile.js
@@ -2,16 +2,37 @@ var fs = require('fs');
 
 module.exports = function(grunt) {
   grunt.initConfig({
-    imagemin: {
-      jpg: {
-        options: { progressive: true },
-        files: {
-          'production/img/chess.jgp': 'production/img/chess-small.jpg',
-          'production/img/hf-trading.jgp': 'production/img/hf-trading-small.jpg',
-          'production/img/movie-studio.jgp': 'production/img/movie-studio-small.jpg'
-        }
+    /* Check HTML does not contain any errors */
+    validation: {
+      options: {
+        doctype: 'HTML5',
+        stoponerror: true,
+        relaxerror: ['Empty heading.']
+      },
+      files: {
+        src: ['index.html']
       }
     },
+    /* Check CSS does not contain any errors */
+    csslint: {
+      strict: {
+        src: ['css/style.css']
+      }
+    },
+    /* Remove whitespace from the HTML */
+    htmlmin: {
+      options: { collapseWhitespace: true },
+      target: {
+        files: { 'production/index.html': 'index.html' }
+      }
+    },
+    /* Remove whitespace from the CSS */
+    cssmin: {
+      target: {
+        files: { 'production/css/style.min.css': 'css/style.css' }
+      }
+    },
+    /* Make the images smaller */
     responsive_images: {
       ostask: {
         options: {
@@ -30,30 +51,15 @@ module.exports = function(grunt) {
         }]
       }
     },
-    cssmin: {
-      target: {
-        files: { 'production/css/style.min.css': 'css/style.css' }
-      }
-    },
-    htmlmin: {
-      options: { collapseWhitespace: true },
-      target: {
-        files: { 'production/index.html': 'index.html' }
-      }
-    },
-    validation: {
-      options: {
-        doctype: 'HTML5',
-        stoponerror: true,
-        relaxerror: ['Empty heading.']
-      },
-      files: {
-        src: ['index.html']
-      }
-    },
-    csslint: {
-      strict: {
-        src: ['css/style.css']
+    /* Compress the images */
+    imagemin: {
+      jpg: {
+        options: { progressive: true },
+        files: {
+          'production/img/chess.jgp': 'production/img/chess-small.jpg',
+          'production/img/hf-trading.jgp': 'production/img/hf-trading-small.jpg',
+          'production/img/movie-studio.jgp': 'production/img/movie-studio-small.jpg'
+        }
       }
     }
   });
@@ -65,12 +71,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-w3c-html-validation');
   grunt.loadNpmTasks('grunt-contrib-csslint');
 
-  grunt.registerTask('imgopt', ['imagemin']);
-  grunt.registerTask('sizeopt', ['responsive_images']);
-  grunt.registerTask('cssopt', ['cssmin']);
-  grunt.registerTask('htmlopt', ['htmlmin']);
+  /* Note that some of these have dependencies, and thus will not work unless
+   * their dependencies are executed first
+   */
   grunt.registerTask('valid', ['validation']);
   grunt.registerTask('css', ['csslint']);
+  grunt.registerTask('htmlopt', ['htmlmin']);
+  grunt.registerTask('cssopt', ['cssmin']);
+  grunt.registerTask('sizeopt', ['responsive_images']);
+  grunt.registerTask('imgopt', ['imagemin']);
   grunt.registerTask('clean', 'Remove temporary files', function() {
     fs.unlink('production/img/chess-small.jpg');
     fs.unlink('production/img/hf-trading-small.jpg');
