efa025390c9ad29ab9707120df1e460360d988af
Omar Serrano <oser520@gmail.com>
2016-10-02 12:02:21 -0700

    Add whitespace in doc string

--- fswd/proj3/main.py
+++ fswd/proj3/main.py
@@ -10,12 +10,14 @@ class BlogItem():
     """
     def __init__(self, title, user, date, likes, intro):
         """Initializes a blog item.
+
         Args:
             title: The blog's title.
             user: The blog's author.
             date: The date published.
             likes: The number of people who like the blog.
             intro: The first few sentences of the blog.
+
         TODO: Use the datastore models to initialize these variables.
         """
         self.title = title
