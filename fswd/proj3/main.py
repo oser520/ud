00ce2c5f05dbb0b8bf1e16fe06acae9b79834ebb
Omar Serrano <oser520@gmail.com>
2016-12-27 18:48:48 -0800

    Remove DeleteCommentHandler

--- fswd/proj3/main.py
+++ fswd/proj3/main.py
@@ -203,12 +203,6 @@ class CreateCommentHandler(webapp2.RequestHandler):
             # TODO: Handle error
             return
 
-class DeleteCommentHandler(webapp2.RequestHandler):
-    """Handle requests to delete a comment on a blog."""
-    def get(self):
-        """TODO: implement"""
-        self.response.out.write('DeleteCommentHandler not implemented yet')
-
 class DeleteBlogHandler(webapp2.RequestHandler):
     """Handle requests to delete a blog."""
     def get(self):
