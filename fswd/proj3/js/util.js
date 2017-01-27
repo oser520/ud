a4db568dcf24a3d331ba4c21c871c30186eca69f
Omar Serrano <oser520@gmail.com>
2017-01-26 22:04:05 -0800

    Add warning message when username is already taken

--- fswd/proj3/js/util.js
+++ fswd/proj3/js/util.js
@@ -15,96 +15,40 @@ function addEvent(el, event, callback) {
   }
 }
 
-/* May enable or disable behavior or style on an element by adding a class to
- * an element.
- * @param flag A boolean flag that determines whether a class name should be
- * added to an element.
- * @param id The id of the input element where the class name may be added.
- * @param className The name of the class.
- */
-/*
-function enableClass(flag, id, className) {
-  var cl = document.getElementById(id).classList;
-  if (flag && cl.contains(className)
-    cl.remove(className);
-  else if (!flag && !cl.contains(className)
-    cl.add(className);
-}
-*/
-
-/* Checks that the username is valid.
- * @detail The username is valid if it contains between 3 and 35 alphanumeric
- * characters, a dot, or an underscore, and it begins with a letter.
- * @param formEl The form element where the user input is located.
- * @param id The id of the form element.
- * @param className The name of the class that enables a warning on the
- * username.
- * @return True if the username is valid, false otherwise.
- */
-/*
-function checkUsername(formEl, id, className) {
-  var val = el.elements.user.value;
-  var valid = /^[a-z][a-z\d._]{3,35}$/.test(val);
-  enableClass(valid, id, className);
-  return valid;
-}
-*/
-
-/* Checks that the password is valid.
- * @detail The password is valid if it contains between 6 and 35 non-white space
- * characters, at least one digit, and at least one letter.
- * @param formEl The element where the password input is located.
- * @param id The id of the input element that may need a warning.
- * @param className The name of the class that may be added as an attribute.
- * @return True if the password is valid, false otherwise.
- */
-/*
-function checkPassword(formEl, id, className) {
-  var val = el.elements.password.value;
-  var valid = /^\S{6,35}$/.test(val) && /\d/.test(val) && /[a-z]/.test(val);
-  enableClass(valid, id, className);
-  return valid;
-}
-*/
-
-/* Checks the username and passwords are valid.
- * @detail Enables warnings if the username or password are not valid, or
- * submits the form if they are.
- * @param e The event whose default behavior we want to stop.
-function checkRegister(e) {
-  e.preventDefault();
-  validUser = checkUsername(this, 'input-username', 'warning-username');
-  validPwd = checkPassword(this, 'input-password', 'warning-password');
-  if (usercheck.good && pwdcheck.good)
-    this.submit();
-}
- */
+(function clearWarning() {
+  var nameBox = document.getElementById('inputUsername');
+  addEvent(nameBox, 'input', function() {
+    var cl = document.getElementById('warning-msg').classList;
+    if (!cl.contains('hidden-warning')) {
+      cl.add('hidden-warning');
+    }
+  });
+})();
 
 // TODO: should only use this on registration form. Need something else for
 // login form.
 (function doRegister() {
   var form = document.getElementById('account-form');
   addEvent(form, 'submit', function(e) {
-    console.log('caught submit request')
     e.preventDefault();
     var xhr = new XMLHttpRequest();
     xhr.onreadystatechange = function() {
       if (xhr.readyState == 4 && xhr.status == 200) {
-        console.log('Got a response from the server:');
-        console.log(xhr.responseText);
         var response = JSON.parse(xhr.responseText);
-        if (response.good) {
+        if (response.success) {
           window.location.assign('/');
         } else {
-          var cl = document.querySelector('.hidden-warning').classList;
+          document.getElementById('username-taken').innerText = response.user;
+          var cl = document.getElementById('warning-msg').classList;
           cl.remove('hidden-warning');
+          var nameBox = document.getElementById('inputUsername');
+          nameBox.value = '';
+          nameBox.focus();
         }
       }
     };
     var user = form.elements.user.value;
     var pwd = form.elements.password.value;
-    console.log('username = ' + user);
-    console.log('pwd      = ' + pwd);
     var msg = JSON.stringify({'user': user, 'password': pwd});
     xhr.open('POST', '/do-register', true);
     xhr.send(msg);
